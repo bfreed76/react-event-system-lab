431383c7aacc1a861a161af9a55fd520206746a8
Brinton <brintonfoyreed@gmail.com>
2021-05-06 15:43:27 -0700

    Assignment Finished

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+
+    render() { 
+        return (  
+            <button 
+            className="eyesOnMe"
+            onFocus={() => console.log('Good!')}
+            onBlur={() => console.log('Hey! Eyes on me!')}
+
+            >Eyes on me Please!</button>
+        );
+    }
+}
+ 
+export default EyesOnMe;
